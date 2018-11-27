506059e93f2102c24905e1fbeb92849654483c68
Wouter Legiest <wouter.legiest@me.com>
2018-11-27 22:02:15 +0100

    Thread op een betere manier

--- Client/src/main/java/threads/ScoreBoardThread.java
+++ Client/src/main/java/threads/ScoreBoardThread.java
@@ -3,45 +3,40 @@ package threads;
 import controller.LobbyController;
 import javafx.application.Platform;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class ScoreBoardThread extends Thread{
 
     private final LobbyController lc;
-    private boolean refresh = false;
+    private AtomicBoolean running = new AtomicBoolean(false);
 
     public ScoreBoardThread(LobbyController lc) {
         this.lc = lc;
     }
 
-    public void stopThread(){
-        refresh=false;
+    public void stopThread() {
+        running.set(false);
     }
-
     public synchronized void run(){
 
-        refresh = true;
+        running.set(true);
 
         try {
-            while(refresh){
+            while(running.get()){
 
                 wait(5000);
 
-                //
+                //Gewone threads classen met JavaFx
                 Platform.runLater(new Runnable() {
                     @Override public void run() {
-                        System.out.println("Uitgevoerd");
+                        //System.out.println("Uitgevoerd");
                         lc.refreshT1();
                         //lc.refreshT2();
                     }
                 });
-
-
-
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
-
-
 }
